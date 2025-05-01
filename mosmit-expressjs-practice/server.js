9981746ffd631d3aff9c62affc1d59ba992f0210
mosmit <sirmosmit10@gmail.com>
2025-05-01 12:32:07 +0100

    added-an-ejs-engine-to-expressjs

--- mosmit-expressjs-practice/server.js
+++ mosmit-expressjs-practice/server.js
@@ -1,4 +1,11 @@
 const express = require("express")
 const app = express()
 
+app.set("view engine", "ejs")
+
+app.get("/", (req, res)=>{
+    console.log("were")
+    res.render("index")
+})
+
 app.listen(3000)
